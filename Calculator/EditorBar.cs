c7fb7957b88ca9c0b0c69c63e2524093b65dfc12
zakorgy <zakorgy@inf.u-szeged.hu>
2017-11-19 20:38:12 +0100

    Add business logic, animation and file write

---
+++ Calculator/EditorBar.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    class EditorBar : INotifyPropertyChanged
+    {
+        #region ClassAttributes
+        /// <summary>
+        /// The result of the actual computation
+        /// </summary>
+        public double Result { get; set; }
+
+        /// <summary>
+        /// Boolen for check if we printed a computation result, and the next button hit should clear the cached computation
+        /// </summary>
+        private bool clearCachedForNextInput;
+
+
+        private string editorBarContent;
+
+
+        /// <summary>
+        /// The actually displayed content in the UI TextBox, this is displayed in the lower textbox
+        /// </summary>
+        public string EditorBarContent
+        {
+            get { return editorBarContent; }
+            set
+            {
+                editorBarContent = value;
+                PropertyChange("EditorBarContent");
+            }
+        }
+
+        /// <summary>
+        /// A string containing the actual computation, this is displayed in the upper textbox
+        /// </summary>
+        private string cachedComputation { get; set; }
+
+        public string CachedComputation
+        {
+            get { return cachedComputation; }
+            set
+            {
+                cachedComputation = value;
+                PropertyChange("CachedComputation");
+            }
+        }
+
+        /// <summary>
+        /// A list containing the finished computations
+        /// </summary>
+        public List<string> ContentToWrite { get; set; }
+        #endregion
+        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public EditorBar()
+        {
+            Result = 0f;
+            EditorBarContent = "";
+            CachedComputation = "";
+            ContentToWrite = new List<string>();
+            clearCachedForNextInput = false;
+        }
+
+        #region MyMethods
+
+        /// <summary>
+        /// Handle the input from the UI (numbers and computation symbols).
+        /// This method is always called if a button is clicked, except for save button.
+        /// </summary>
+        /// <param name="input"></param>
+        public void HandleNextInput(string input)
+        {
+            if(clearCachedForNextInput)
+            {
+                CachedComputation = "";
+                clearCachedForNextInput = false;
+            }
+            switch (input)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (EditorBarContent.Length < 10)
+                    {
+                        EditorBarContent += input;
+                    }
+                    break;
+                case "DEL":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        EditorBarContent = EditorBarContent.Remove(EditorBarContent.Length - 1);
+                    }
+                    break;
+                case ".":
+                    if (!EditorBarContent.Contains("."))
+                    {
+                        if (EditorBarContent.Length > 0)
+                        {
+                            EditorBarContent += input;
+                        }
+                        else
+                        {
+                            EditorBarContent += "0.";
+                        }
+                    }
+                    break;
+                case "÷":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        EditorBarContent = "";
+                        CachedComputation += " / ";
+                    }
+                    break;
+                case "x":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        EditorBarContent = "";
+                        CachedComputation += " * ";
+                    }
+                    break;
+                case "-":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        EditorBarContent = "";
+                        CachedComputation += " - ";
+                    }
+                    break;
+                case "+":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        EditorBarContent = "";
+                        CachedComputation += " + ";
+                    }
+                    break;
+                case "√":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        CachedComputation += " sqrt";
+                        Result = Evaluate(CachedComputation);
+                        CachedComputation += "=" + FirstNineCaharcters(Result);
+                        ContentToWrite.Add(CachedComputation);
+                        CachedComputation = "";
+                        EditorBarContent = FirstNineCaharcters(Result);
+                    }
+                    break;
+                case "1/x":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        CachedComputation += " inv";
+                        Result = Evaluate(CachedComputation);
+                        CachedComputation += "=" + FirstNineCaharcters(Result);
+                        ContentToWrite.Add(CachedComputation);
+                        CachedComputation = "";
+                        EditorBarContent = FirstNineCaharcters(Result);
+                    }
+                    break;
+                case "±":
+                    if (EditorBarContent.Length > 0 && EditorBarContent != "0")
+                    {
+                        //Debug.WriteLine("EditorBarContent[0] = " + EditorBarContent[0]);
+                        if (EditorBarContent[0] == '-')
+                        {
+                            EditorBarContent = EditorBarContent.TrimStart(new char[] { '-' });
+                        }
+                        else
+                        {
+                            EditorBarContent = '-' + EditorBarContent;
+                        }
+                    }
+                    break;
+                case "CE":
+                    EditorBarContent = "";
+                    break;
+                case "C":
+                    CachedComputation = "";
+                    EditorBarContent = "";
+                    break;
+                case "=":
+                    if (EditorBarContent.Length > 0)
+                    {
+                        CachedComputation += EditorBarContent;
+                        //Debug.WriteLine("Computation = {}", CachedComputation);
+                        Result = Evaluate(CachedComputation);
+                        CachedComputation += "=" + FirstNineCaharcters(Result);
+                        ContentToWrite.Add(CachedComputation);
+                        EditorBarContent = FirstNineCaharcters(Result);
+                        clearCachedForNextInput = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// Formatting function for only printing the first nine characters to the screen
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        string FirstNineCaharcters(double input)
+        {
+            var stringToTruncate = input.ToString("0.########");
+            var result = !String.IsNullOrWhiteSpace(stringToTruncate) && stringToTruncate.Length >= 9
+                ? stringToTruncate.Substring(0, 9)
+                : stringToTruncate;
+            return result;
+        }
+
+        /// <summary>
+        /// Evaluate the input string as a mathematical expression
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static double Evaluate(String input)
+        {
+            Stack<String> ops = new Stack<String>();
+            Stack<Double> vals = new Stack<Double>();
+
+            String bracket = "( " + input + " )";
+            var expr = bracket.Split(new string[] { " " }, StringSplitOptions.None);
+            for (int i = 0; i < expr.Length; i++)
+            {
+                String s = expr[i];
+                Debug.WriteLine("String = " + s);
+                if (s.Equals("(")) { }
+                else if (s.Equals("+")) ops.Push(s);
+                else if (s.Equals("-")) ops.Push(s);
+                else if (s.Equals("*")) ops.Push(s);
+                else if (s.Equals("/")) ops.Push(s);
+                else if (s.Equals("sqrt")) ops.Push(s);
+                else if (s.Equals("inv")) ops.Push(s);
+                else if (s.Equals(")"))
+                {
+                    int count = ops.Count;
+                    while (count > 0)
+                    {
+                        String op = ops.Pop();
+                        double v = vals.Pop();
+                        if (op.Equals("+")) v = vals.Pop() + v;
+                        else if (op.Equals("-")) v = vals.Pop() - v;
+                        else if (op.Equals("*")) v = vals.Pop() * v;
+                        else if (op.Equals("/")) v = vals.Pop() / v;
+                        else if (op.Equals("sqrt")) v = Math.Sqrt(v);
+                        else if (op.Equals("inv")) v = 1f / v;
+                        vals.Push(v);
+
+                        count--;
+                    }
+                }
+                else vals.Push(Double.Parse(s));
+            }
+            return vals.Pop();
+        }
+        #endregion
+
+        #region ParentInterfaceMethods
+
+        /// <summary>
+        /// INotifyPropertyChanged interface method to notify the UI the given property is changed in the class.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void PropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +13,68 @@ namespace Calculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        /// <summary>
+        /// EditorBar class is for handling the business logic.
+        /// The button inputs are handled by this class.
+        /// </summary>
+        EditorBar eBar;
+
         public MainPage()
         {
             this.InitializeComponent();
+            eBar = new EditorBar();
+            this.DataContext = eBar;
         }
 
-        private void ButtonDel_Copy3_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// This method is called when a button is clicked on the UI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            // Stop the animation if it still runs on a previous button.
+            // This is required for SetValue.
+            ButtonAnimationStoryBoard.Stop();
+            
+            // Convert the sender object to a button.
+            Button button = (Button)sender;
+            
+            // Set the TargetNameProperty of the animation to the name of the pressed button.
+            ButtonAnimation.SetValue(Storyboard.TargetNameProperty, button.Name);
+           
+            // Start the animation on the button.
+            ButtonAnimationStoryBoard.Begin();
+            
+            // Handle the Conent of the button in the EditorBar class.
+            eBar.HandleNextInput(button.Content.ToString());
         }
+
+        /// <summary>
+        /// Async method to write the cached calculations into a text file.
+        /// This is triggered when the 'S' button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Start the animation for this button.
+            SaveButtonAnimation.Begin();
+
+            // Create folder instance
+            Windows.Storage.StorageFolder folder =  Windows.Storage.ApplicationData.Current.LocalFolder;
+            
+            // Create a new file to write, or open if it exists.
+            Windows.Storage.StorageFile sampleFile =
+                await folder.CreateFileAsync("calculations.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            
+            // Append the cached computation at the end of the file
+            await Windows.Storage.FileIO.AppendLinesAsync(sampleFile, eBar.ContentToWrite);
+           
+            // Tell the user, that saving is done.
+            Windows.UI.Popups.MessageDialog md = new Windows.UI.Popups.MessageDialog("Results saved to file");
+            await md.ShowAsync();
+        }        
     }
 }
